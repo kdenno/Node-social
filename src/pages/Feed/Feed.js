74525bcce1aa1e1904cf5ed0703aff14c560242d
kdenno <kabuyedennis3@gmail.com>
2020-03-09 21:14:43 +0300

    updating posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -59,7 +59,12 @@ class Feed extends Component {
       })
       .then(resData => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map(post => {
+            return {
+              ...post,
+              imagePath: post.imageUrl
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false
         });
@@ -107,19 +112,19 @@ class Feed extends Component {
     });
     // Set up data (with image!)
     let url = "http://localhost:8080/feed/post";
-  let method = 'POST';
+    let method = "POST";
     if (this.state.editPost) {
-      url = "URL";
+      url = "http://localhost:8080/feed/post" + this.state.editPost._id;
+      method = "PUT";
     }
     const formData = new FormData(); // browser formData object
-    formData.append('title', postData.title);
-    formData.append('content', postData.content);
-    formData.append('image', postData.image);
+    formData.append("title", postData.title);
+    formData.append("content", postData.content);
+    formData.append("image", postData.image);
 
     fetch(url, {
       method: method,
       body: formData
-
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
