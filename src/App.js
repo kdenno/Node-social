e8359b9ad3418d3b13f4400d16fc6491176037d9
kdenno <kabuyedennis3@gmail.com>
2020-03-10 18:48:05 +0300

    authenticating users with JWT

--- src/App.js
+++ src/App.js
@@ -59,7 +59,16 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch("URL")
+    fetch("http://localhost:8080/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error("Validation failed.");
@@ -100,7 +109,7 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch("http://localhost:8080/feed/signup", {
+    fetch("http://localhost:8080/auth/signup", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json"
